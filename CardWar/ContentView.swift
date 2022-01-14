b41900b59f80baffed5c610e1d06b005ed915979
Jasleen-1410 <jasleen.minhas07@gmail.com>
2022-01-15 00:02:26 +0530

    First Commit

--- CardWar/ContentView.swift
+++ CardWar/ContentView.swift
@@ -1,17 +1,99 @@
-//
-//  ContentView.swift
-//  CardWar
-//
-//  Created by Apple on 14/01/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "back"
+    @State var cpuCard = "back"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack{
+            Image("background").ignoresSafeArea()
+            
+            VStack(spacing: 0.0){
+                Spacer()
+                
+                Image("logo")
+
+                Spacer()
+
+                HStack(){
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+
+                VStack(){
+                    
+                    Button(action: {
+                        let playerRand = Int.random(in: 2...14)
+                        let cpuRand = Int.random(in: 2...14)
+                        
+                        playerCard = "card"+String(playerRand)
+                        cpuCard = "card"+String(cpuRand)
+                        
+                        // Update score
+                        if playerRand == cpuRand {
+                            playerScore += 1
+                            cpuScore += 1
+                        }
+                        else if playerRand > cpuRand {
+                            playerScore += 1
+                        }
+                        else{
+                            cpuScore += 1
+                        }
+                        
+                    },
+                           label: {
+                            Image("dealbutton")
+                    })
+                }
+
+                Spacer()
+                
+                HStack(){
+                    Spacer()
+                    
+                    VStack(spacing: 5.0){
+                        Text("USER")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .foregroundColor(Color.white)
+                            .font(.title)
+                    }.padding()
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 5.0){
+                        Text("CPU")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .foregroundColor(Color.white)
+                            .font(.title)
+                    }.padding()
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+        }
     }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
