6f02c44650d1f4c4a941dcd46b883e6ca81ef08f
Jasleen-1410 <jasleen.minhas07@gmail.com>
2022-01-14 23:37:56 +0530

    Initial Commit

---
+++ CardWar/CardWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWarApp.swift
+//  CardWar
+//
+//  Created by Apple on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CardWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
